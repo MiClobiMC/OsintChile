e5ad508205c3b9fbc7b1a2b01c637a1e570a9320
MiClobiMC <60762569+MiClobiMC@users.noreply.github.com>
2022-07-10 23:37:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+import sources.nombrerutyfirma as nrf
+import sources.salud as salud
+import sources.edadrut as edadrut
+import sources.texto as texto
+import sources.sii as sii
+import sources.volanteomaleta as volanteomaleta
+
+parser = argparse.ArgumentParser(prog='doxingchile', description='Busqueda automatica en fuentes abiertas (y no tan abiertas) de chile')
+parser.add_argument('-rut',  type=str, nargs='?', help='Rut de la persona a buscar, con formato: 11111111-1 ')
+
+parametros = parser.parse_args()
+
+parametros.rut = input("Ingrese el rut de la persona: ")
+
+if not(parametros.rut):
+    parser.print_help()
+
+if parametros.rut:
+    texto.busqueda(rut = parametros.rut)
+    salud.busqueda(rut = parametros.rut)
+    edadrut.busqueda(rut = parametros.rut)
+    nrf.busqueda(rut = parametros.rut )
+    sii.busqueda(rut = parametros.rut)
+    volanteomaleta.busqueda(rut = parametros.rut)
